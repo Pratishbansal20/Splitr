cf5136f3a66b4ca3f64da3c79f7ee2581d341328
Pratishbansal20 <pratishbansal2017@gmail.com>
2026-02-26 00:35:04 +0530

    fix: append /api to production baseURL

--- frontend-redone-full/src/api.js
+++ frontend-redone-full/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://splitr-2g63.onrender.com/',
+  baseURL: 'https://splitr-2g63.onrender.com/api',
   withCredentials: false,
 });
 
