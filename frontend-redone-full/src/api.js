7676ab62faddc0537dc67b0a1bc7efbca5e64d2c
Pratishbansal20 <pratishbansal2017@gmail.com>
2026-02-26 00:07:38 +0530

    chore: point frontend to production backend

--- frontend-redone-full/src/api.js
+++ frontend-redone-full/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Change if your backend uses a different URL or port
+  baseURL: 'https://splitr-2g63.onrender.com/',
   withCredentials: false,
 });
 
